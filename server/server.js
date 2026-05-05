fffd951e8c5c7399b8956b27970d87d8eb012e63
Aflah Syamsul Awaludin <aflahsyamsul02@gmail.com>
2026-05-05 21:34:16 +0700

    Fix catch-all route with regex group2

--- server/server.js
+++ server/server.js
@@ -37,9 +37,9 @@ if (!MONGODB_URI) {
 // Serve static assets in production
 const path = require('path');
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/dist')));
+  app.use(express.static(path.join(__dirname, '../dist')));
   app.get(/.*/, (req, res) => { 
-    res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../dist', 'index.html'));
   });
 }
 app.listen(PORT, () => {
