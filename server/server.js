9bd3005eead3184b8606989acd53e53cf9234a11
Aflah Syamsul Awaludin <aflahsyamsul02@gmail.com>
2026-05-05 21:26:38 +0700

    fix: use regex /.*/ for catch-all route to resolve Express 5 path-to-regexp error

--- server/server.js
+++ server/server.js
@@ -38,7 +38,7 @@ if (!MONGODB_URI) {
 const path = require('path');
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
-  app.get('(.*)', (req, res) => { 
+  app.get(/.*/, (req, res) => { 
     res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
   });
 }
